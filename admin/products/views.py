493ccd5bd187e3423f7e768ddcf064c60cd90e52
Federico <federicolucaspontoriero@gmail.com>
2022-12-12 17:20:00 -0300

    added initial docker setup

---
+++ admin/products/views.py
@@ -0,0 +1,20 @@
+from rest_framework import viewsets
+
+# Create your views here.
+
+
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request):  # /api/products
+        pass
+
+    def create(self, request):
+        pass
+
+    def retrieve(self, request, pk=None):  # /api/products/<str:id>
+        pass
+
+    def update(self, request, pk=None):  # /api/products/<str:id>
+        pass
+
+    def delete(self, request, pk=None):  # /api/products/<str:id>
+        pass
