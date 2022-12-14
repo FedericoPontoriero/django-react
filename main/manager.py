cafdb9dd9bbcd96498b1f3bbbba8be0bda07a65a
Federico <federicolucaspontoriero@gmail.com>
2022-12-13 23:38:14 -0300

    updated for flask new version

--- main/manager.py
+++ main/manager.py
@@ -1,11 +1,11 @@
 from main import app, db
-from flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager
+from flask_migrate import Migrate
+from flask.cli import FlaskGroup
 
-migrate = Migrate(app, db)
+cli = FlaskGroup(app, db)
 
-manager = Manager(app)
-manager.add_command("db", MigrateCommand)
+migrate = Migrate()
+migrate.init_app(app, db)
 
 if __name__ == "__main__":
-    manager.run()
+    cli()
