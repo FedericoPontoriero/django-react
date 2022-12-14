fe0afe5b5a26db80203b0b06795003d9d84a62f1
Federico <federicolucaspontoriero@gmail.com>
2022-12-14 19:44:15 -0300

    added main components

--- react-crud/src/admin/Products.tsx
+++ react-crud/src/admin/Products.tsx
@@ -1,27 +1,48 @@
+import { useEffect, useState } from "react";
+import { Product } from "../interfaces/product";
+import Wrapper from "./Wrapper";
+
 const Products = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const response = await fetch("http://localhost:8000/api/products");
+            const data = await response.json();
+            setProducts(data);
+        })();
+    }, []);
+
     return (
-        <div className="table-responsive">
-            <table className="table table-striped table-sm">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Header</th>
-                        <th scope="col">Header</th>
-                        <th scope="col">Header</th>
-                        <th scope="col">Header</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Header</th>
-                        <th scope="col">Header</th>
-                        <th scope="col">Header</th>
-                        <th scope="col">Header</th>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+        <Wrapper>
+            <div className="table-responsive">
+                <table className="table table-striped table-sm">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Image</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Likes</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {products.map((p: Product) => {
+                            return (
+                                <tr key={p.id}>
+                                    <td scope="col">
+                                        <img src={p.image} height="180" />
+                                    </td>
+                                    <td scope="col">{p.title}</td>
+                                    <td scope="col">{p.likes}</td>
+                                    <td scope="col"></td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </div>
+        </Wrapper>
     );
 };
 
